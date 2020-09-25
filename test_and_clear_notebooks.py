c1dbd3a49ff52f681569eac487b2656d0da03448
Michael Cullan <mjcullan@gmail.com>
2020-09-25 11:20:20 -0400

    testing

--- test_and_clear_notebooks.py
+++ test_and_clear_notebooks.py
@@ -7,6 +7,26 @@ from nbconvert import HTMLExporter
 from nbconvert.preprocessors import ClearOutputPreprocessor, ExecutePreprocessor
 from nbconvert.preprocessors import CellExecutionError
 
+def get_tags_flat(nb):
+    cells = nb['cells']
+    tags = [cell['metadata'].get('tags') for cell in cells]
+    tags = [tag for tag in tags if tag]
+
+    tags_flat = [tag for cell_tag in tags for tag in cell_tag]
+
+    return tags_flat
+
+def save_html(nb, notebook_filename, html_directory):
+    html_exporter = HTMLExporter()
+    html_exporter.template_name = 'classic'
+
+    html_filename = notebook_filename.replace('.ipynb', '.html')
+    html_destination = os.path.join(html_directory, html_filename)
+
+    with open(html_destination, 'w') as f:
+        html_out, resources = html_exporter.from_notebook_node(nb)
+        f.write(html_out)
+
 def process_notebook(notebook_filename, html_directory = 'notebook-html', execute=True):
     '''Checks if an IPython notebook runs without error from start to finish. If so, writes the notebook to HTML (with outputs) and overwrites the .ipynb file (without outputs).
     '''
@@ -25,6 +45,12 @@ def process_notebook(notebook_filename, html_directory = 'notebook-html', execut
         msg = f'\n  Error executing the notebook "{notebook_filename}".\n'
         msg += f'  See notebook "{notebook_filename}" for the traceback.'
     finally:
+        tags_flat = get_tags_flat(nb)
+
+        if 'save_html' in tags_flat:
+            print("Writing html")
+            save_html(nb, notebook_filename, html_directory)
+
         # Clear notebook outputs and save as .ipynb
         cleared = clear.preprocess(nb, {})
         with open(notebook_filename, mode='w', encoding='utf-8') as f:
@@ -34,26 +60,6 @@ def process_notebook(notebook_filename, html_directory = 'notebook-html', execut
     return
     
 
-        
-def process_all_notebooks():
-    '''Runs `process_notebook` on all notebooks in the git repository.
-    '''
-    # Get all files included in the git repository
-    git_files = (subprocess
-                 .check_output("git diff --name-only", shell=True)
-                 .decode('utf-8')
-                 .splitlines())
-
-    # Get just the notebooks from the git files
-    notebooks = [fn for fn in git_files if fn.endswith(".ipynb") if fn != 'Notebook-testing-demo.ipynb']
-  
-    
-    # Test each notebook, save it to HTML with outputs, and clear the outputs from the .ipynb file
-    for notebook in notebooks:
-        print(f"Now processing {notebook}")
-        process_notebook(notebook)
-        
-    return notebooks
 
 if __name__ == '__main__':
     
