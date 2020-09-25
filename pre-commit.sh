d1ccfda4f88965fde26877b8e925279fb709372d
Michael Cullan <mjcullan@gmail.com>
2020-09-25 11:06:32 -0400

    modified pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,7 +4,6 @@
 STASH_NAME="pre-commit-$(date +%s)"
 git stash save  --include-untracked -q --keep-index $STASH_NAME
 
-echo "running pre commit"
 
 # Test prospective commit
 num_notebooks=`git diff --cached --name-only | grep  -c .ipynb`
@@ -13,6 +12,7 @@ notebooks=`git diff --cached --name-only | grep  .ipynb`
 if [ ${num_notebooks} -eq 0 ]; then
      echo "No notebooks"
  else
+ 	echo "processing notebooks: $notebooks"
      python test_and_clear_notebooks.py $notebooks
  fi
 git add .
