050ac33ca104687c2bfff66dc7a1d5ffb57da674
Michael Cullan <mjcullan@gmail.com>
2020-09-25 09:09:30 -0400

    pre commit script at root

---
+++ pre-commit.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# An example hook script to verify what is about to be committed.
+# Called by "git commit" with no arguments.  The hook should
+# exit with non-zero status after issuing an appropriate message if
+# it wants to stop the commit.
+#
+# To enable this hook, rename this file to "pre-commit".
+# pre-commit.sh
+STASH_NAME="pre-commit-$(date +%s)"
+git stash save -q --keep-index $STASH_NAME
+
+# Test prospective commit
+python process_notebooks.py
+
+git add .
+
+STASHES=$(git stash list)
+if [[ $STASHES == "$STASH_NAME" ]]; then
+  git stash pop -q
+fi
+
