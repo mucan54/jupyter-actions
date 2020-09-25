8dc2e5dff0beac81d3e4024ecd91ff82abeb1516
Michael Cullan <mjcullan@gmail.com>
2020-09-25 11:31:54 -0400

    html

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,7 +7,7 @@ git stash save  --include-untracked -q --keep-index $STASH_NAME
 
 # Test prospective commit
 num_notebooks=`git diff --cached --name-only --diff-filter=d | grep  -c .ipynb`
-notebooks=`git diff --cached --name-only | grep  .ipynb`
+notebooks=`git diff --cached --name-only --diff-filter=d| grep  .ipynb`
 
 if [ ${num_notebooks} -eq 0 ]; then
      echo "No notebooks to process."
